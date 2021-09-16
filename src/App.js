747f46e7d47e4884f0a6a4d99e58b9980b1e4794
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-09-16 13:03:51 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome To CI/CD Pipeline For React</h1>
+      <h4>In this we automate the Deployment of the react app (CD)</h4>
+    </div>
+  );
+}
+
+export default App;
